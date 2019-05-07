e486fbe6bbf10587b253034436619aa019e3c1ae
Kirlat <kirlat@yula.media>
2019-05-07 10:33:26 +0400

    Deleted a private configuration file

--- alpheios_nemo_ui/data/assets/js/env.js
+++
@@ -1,7 +0,0 @@
-const serverEnv = {
-    SESSION_URL: 'http://dev.alpheios.net:5000/userinfo',
-    TOKEN_URL: 'http://dev.alpheios.net:5000/usertoken',
-    ENDPOINTS: {
-      wordlist : 'https://w2tfh159s2.execute-api.us-east-2.amazonaws.com/prod/words',
-    }
-}
