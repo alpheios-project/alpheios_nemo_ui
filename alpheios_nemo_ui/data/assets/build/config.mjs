2280b6bb580e07489994a46ca21741d8fdabecd7
Kirlat <kirlat@yula.media>
2019-06-10 17:49:27 +0400

    Updated webpack's config file to eliminate code that is not used right now

--- alpheios_nemo_ui/data/assets/build/config.mjs
+++ alpheios_nemo_ui/data/assets/build/config.mjs
@@ -1,45 +1,7 @@
-import VueLoaderPlugin from '../node_modules/vue-loader/lib/plugin.js'
-import InjectManifest from '../node_modules/workbox-webpack-plugin/build/inject-manifest.js'
-import WebpackPwaManifest from 'webpack-pwa-manifest'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import Package from '../package.json'
-
-import path from 'path'
-const projectRoot = process.cwd()
-
-const sharedManifestConf = {
-  name: 'My Progressive Web App',
-  short_name: 'MyPWA',
-  description: 'My awesome Progressive Web App!',
-  background_color: '#ffffff',
-  fingerprints: false,
-  inject: false,
-  ios: {
-    'apple-mobile-web-app-title': 'Alpheios Reader',
-    'apple-mobile-web-app-status-bar-style': '#73CDDE'
-  },
-  icons: [
-    {
-      src: path.resolve('images/alpheios_32.png'),
-      sizes: [36, 48, 72, 96, 144, 192, 512],
-      destination: path.join('alpheios_nemo-ui/data/assets/images/icons', 'pwa')
-    },
-    {
-      src: path.resolve('images/alpheios_32.png'),
-      sizes: [120, 152, 167, 180, 1024],
-      destination: path.join('alpheios_nemo-ui/data/assets/images/icons', 'ios'),
-      ios: true
-    }
-  ]
-}
-
 const webpack = {
   common: {
     entry: './dummy.js',
-    externals: { },
-    plugins: [
-      new WebpackPwaManifest(sharedManifestConf)
-    ]
+    externals: { }
   },
 
   production: {
